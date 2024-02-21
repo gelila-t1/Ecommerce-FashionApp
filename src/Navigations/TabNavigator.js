adf2df9ee2751d8fd9c719e4db0d6b77ab23b48d
gelila-t1 <gelilatesfaye13@gmail.com>
2024-02-21 01:48:55 -1200

    material icons added

--- src/Navigations/TabNavigator.js
+++ src/Navigations/TabNavigator.js
@@ -1,16 +1,26 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { CartStackNavigator, MainStackNavigator, OrderStackNavigator, ProfileStackNavigator } from "./StackNavigator";
-
+import MaterialIcons from "react-native-vector-icons/MaterialIcons"
 
 const Tab = createBottomTabNavigator();
 
  export const TabNavigator=()=>{
     return(
-        <Tab.Navigator>
-            <Tab.Screen name="home" component={MainStackNavigator}/>
-            <Tab.Screen name="cart" component={CartStackNavigator}/>
-            <Tab.Screen name="order" component={OrderStackNavigator}/>
-            <Tab.Screen name="profile" component={ProfileStackNavigator}/>
+        <Tab.Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: "black",
+            tabBarInactiveTintColor: "gray"
+        }}>
+            <Tab.Screen name="home" component={MainStackNavigator} 
+            options={{tabBarIcon:({size,color})=>(<MaterialIcons name="home" size={size} color={color}/>)}}/>
+            <Tab.Screen name="cart" component={CartStackNavigator}
+             options={{tabBarIcon:({size,color})=>(<MaterialIcons name="shopping-cart" size={size} color={color}/>)}}/>
+            <Tab.Screen name="order" component={OrderStackNavigator}
+             options={{tabBarIcon:({size,color})=>(<MaterialIcons name="list-alt" size={size} color={color}/>)}}/>
+            <Tab.Screen name="profile" component={ProfileStackNavigator}
+             options={{tabBarIcon:({size,color})=>(<MaterialIcons name="account-circle" size={size} color={color}/>)}}/>
 
             {/* why not just 
             <Tab.Screen name="" component={HomeScreen}/>
