97ceb3e967a1cae06f0bda45d7f93cea83a434a7
gelila-t1 <gelilatesfaye13@gmail.com>
2024-02-21 07:38:11 -1200

    changes on Home screen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,11 +1,53 @@
-import React  from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect }  from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Pressable, StyleSheet, Text, View, Image } from 'react-native' 
+import MaterialIcons from "react-native-vector-icons/MaterialIcons"
+import userImage from "../../assets/userImage.png"
+import { TextInput } from 'react-native-gesture-handler'
 
-
-const HomeScreen=()=>{
+const HomeScreen=({navigation})=>{
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShown: false
+    })
+  },[])
   return (
     <View>
-      <Text> textInComponent </Text>
+<SafeAreaView>
+
+        <View className="flex-row justify-between mt-2 p-5">
+      <View className="w-10 h-10 ">
+      <MaterialIcons name='menu' size={24} color={"black"}/>
+      </View>
+      <View>
+        <Pressable className="flex-row justify-center px-5 py-2 items-center border border-slate-400 rounded-full">
+          <Image source={userImage} className="h-10 w-10"/>
+           <Text className="font-semibold px-2">
+            Login
+          </Text>
+        </Pressable>
+      </View>
+      </View>
+      <View className="ml-4">
+        <Text className="font-bold text-2xl">Welcome ,User</Text>
+        <Text className="font-bold text-2xl text-grey-400"> to Our Fashion App</Text>
+      </View>
+      
+
+      <View className="mt-4">
+      <View className="flex-row bg-gray-200 rounded-2xl px-2 py-2 mx-4">
+        <View>
+        <MaterialIcons name='search' size={24} color={'#111'}/>
+        </View>     
+        <TextInput
+        placeholder='search....'
+        placeholderTextColor={'#666666'}
+        className="px-2"/>
+      </View>
+      </View>
+</SafeAreaView>
+     
+   
     </View>
   )}
 
